9ec9a5e5fad7f02ef7a9b12aa33aa80b3201836f
chensangshen <15779501591@163.com>
2021-12-16 14:56:23 +0800

    add cluster resource controller

--- pkg/controller/cluster_resource/cluster_resource_controller.go
+++ pkg/controller/cluster_resource/cluster_resource_controller.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 	"time"
 
+	"harmonycloud.cn/stellaris/pkg/apis/multicluster/common"
+
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+
 	"github.com/go-logr/logr"
 	"harmonycloud.cn/stellaris/pkg/apis/multicluster/v1alpha1"
 	managerCommon "harmonycloud.cn/stellaris/pkg/common"
@@ -28,7 +33,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 	r.log.Info("Reconciling MultiClusterResourceBinding")
 
 	// get resource
-	instance := &v1alpha1.MultiClusterResourceBinding{}
+	instance := &v1alpha1.ClusterResource{}
 	err := r.Client.Get(ctx, request.NamespacedName, instance)
 	if err != nil {
 		if apierrors.IsNotFound(err) {
@@ -51,6 +56,33 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 
 	// the object is being deleted
 	if !instance.ObjectMeta.DeletionTimestamp.IsZero() && sliceutil.ContainsString(instance.ObjectMeta.Finalizers, managerCommon.FinalizerName) {
+		if !managerCommon.IsControlPlane() {
+			if instance.Status.Phase != common.Terminating {
+				// update status
+				instance.Status.Message = ""
+				instance.Status.ObservedReceiveGeneration = instance.Generation
+				instance.Status.Phase = common.Terminating
+
+				err = updateClusterResourceStatus(ctx, r.Client, instance)
+				if err != nil {
+					return ctrl.Result{
+						Requeue:      true,
+						RequeueAfter: 30 * time.Second,
+					}, fmt.Errorf("update status failed:%s, resource(%s)", err, instance.Name)
+				}
+				return ctrl.Result{}, nil
+			}
+		} else {
+			//
+			err = sendClusterResourceToAgent(SyncEventTypeDelete, instance)
+			if err != nil {
+				return ctrl.Result{
+					Requeue:      true,
+					RequeueAfter: 30 * time.Second,
+				}, fmt.Errorf("send ClusterResouce failed:%s, resource(%s)", err, instance.Name)
+			}
+		}
+
 		instance.ObjectMeta.Finalizers = sliceutil.RemoveString(instance.ObjectMeta.Finalizers, managerCommon.FinalizerName)
 		if err = r.Client.Update(ctx, instance); err != nil {
 			return ctrl.Result{
@@ -58,12 +90,68 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 				RequeueAfter: 30 * time.Second,
 			}, fmt.Errorf("delete finalizer filed from resource %s failed: %s", instance.Name, err)
 		}
+
 		return ctrl.Result{}, nil
 	}
 
-	// TODO send agent clusterResource
+	if managerCommon.IsControlPlane() {
+		// TODO send clusterResource to agent
+		err = sendClusterResourceToAgent(SyncEventTypeUpdate, instance)
+		if err != nil {
+			return ctrl.Result{
+				Requeue:      true,
+				RequeueAfter: 30 * time.Second,
+			}, fmt.Errorf("send ClusterResouce failed:%s, resource(%s)", err, instance.Name)
+		}
+		return ctrl.Result{}, nil
+	} else {
+		if instance.Generation != instance.Status.ObservedReceiveGeneration {
+			// update status to creating
+			instance.Status.ObservedReceiveGeneration = instance.Generation
+			instance.Status.Phase = common.Creating
+			instance.Status.Message = ""
 
-	// TODO sync binding status
+			err = updateClusterResourceStatus(ctx, r.Client, instance)
+			if err != nil {
+				return ctrl.Result{
+					Requeue:      true,
+					RequeueAfter: 30 * time.Second,
+				}, fmt.Errorf("update status failed:%s, resource(%s)", err, instance.Name)
+			}
+			return ctrl.Result{}, nil
+		}
+		if instance.Status.Phase == common.Creating {
+			// create/update resource
+			err = syncResource(ctx, r.Client, instance.Spec.Resource)
+			if err != nil {
+				instance.Status.Message = err.Error()
+
+				err = updateClusterResourceStatus(ctx, r.Client, instance)
+				if err != nil {
+					return ctrl.Result{
+						Requeue:      true,
+						RequeueAfter: 30 * time.Second,
+					}, fmt.Errorf("update status failed:%s, resource(%s)", err, instance.Name)
+				}
+
+				return ctrl.Result{
+					RequeueAfter: 30 * time.Second,
+					Requeue:      true,
+				}, fmt.Errorf("sync resource fail, resource %s failed: %s", instance.Name, instance.Status.Message)
+			}
+			instance.Status.Message = ""
+			instance.Status.Phase = common.Complete
+			instance.Status.ObservedReceiveGeneration = instance.Generation
+
+			err = updateClusterResourceStatus(ctx, r.Client, instance)
+			if err != nil {
+				return ctrl.Result{
+					Requeue:      true,
+					RequeueAfter: 30 * time.Second,
+				}, fmt.Errorf("update status failed:%s, resource(%s)", err, instance.Name)
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -81,4 +169,58 @@ func Setup(mgr ctrl.Manager, controllerCommon controllerCommon.Args) error {
 		log:    logf.Log.WithName("cluster_resource_controller"),
 	}
 	return reconciler.SetupWithManager(mgr)
+}
+
+func syncResource(ctx context.Context, clientSet client.Client, resource *runtime.RawExtension) error {
+	resourceObject, err := formatDataToUnstructured(resource.Raw)
+	if err != nil {
+		return err
+	}
+	err = clientSet.Create(ctx, resourceObject)
+	if err != nil {
+		if !apierrors.IsAlreadyExists(err) {
+			return err
+		}
+		err = clientSet.Update(ctx, resourceObject)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func formatDataToUnstructured(data []byte) (*unstructured.Unstructured, error) {
+	// Decode YAML manifest into unstructured.Unstructured
+	obj := &unstructured.Unstructured{}
+	dec := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	_, _, err := dec.Decode(data, nil, obj)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+func updateClusterResourceStatus(ctx context.Context, clientSet client.Client, clusterResource *v1alpha1.ClusterResource) error {
+	err := sendStatusToControlPlane(&clusterResource.Status)
+	if err != nil {
+		return err
+	}
+	return clientSet.Status().Update(ctx, clusterResource)
+}
+
+func sendStatusToControlPlane(resourceStatus *v1alpha1.ClusterResourceStatus) error {
+	// TODO send status to controlPlane
+	return nil
+}
+
+type SyncEventType string
+
+const (
+	SyncEventTypeUpdate SyncEventType = "update"
+	SyncEventTypeDelete SyncEventType = "delete"
+)
+
+func sendClusterResourceToAgent(eventType SyncEventType, clusterResource *v1alpha1.ClusterResource) error {
+	// TODO send clusterResource to agent
+	return nil
 }
