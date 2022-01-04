85ffd65c18c3bfd62dcf9659893c3266344f3307
chensangshen <15779501591@163.com>
2022-01-04 19:56:52 +0800

    add clusterResource and aggregateRule webhook

---
+++ pkg/common/validation/validation.go
@@ -0,0 +1,20 @@
+package validation
+
+import (
+	"fmt"
+
+	kubevalidationpath "k8s.io/apimachinery/pkg/api/validation/path"
+)
+
+const clusterResourceNameMaxLength int = 64
+
+func ValidateClusterResourceName(name string) []string {
+	if len(name) > clusterResourceNameMaxLength {
+		return []string{fmt.Sprintf("must be no more than %d characters", clusterResourceNameMaxLength)}
+	}
+	return kubevalidationpath.IsValidPathSegmentName(name)
+}
+
+func ValidateAggregateRule() {
+
+}
