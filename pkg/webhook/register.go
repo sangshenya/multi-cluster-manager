1ca0716bef409c41dbffef5d1582dfb43d69434d
chensangshen <15779501591@163.com>
2022-01-12 16:30:58 +0800

    add agent deploy file fix core deploy file

--- pkg/webhook/register.go
+++ pkg/webhook/register.go
@@ -4,6 +4,7 @@ import (
 	controllerCommon "harmonycloud.cn/stellaris/pkg/controller/common"
 	"harmonycloud.cn/stellaris/pkg/webhook/cluster_resource"
 	"harmonycloud.cn/stellaris/pkg/webhook/cluster_resource_aggregate_rule"
+	"harmonycloud.cn/stellaris/pkg/webhook/multi_cluster_resource"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
@@ -11,7 +12,11 @@ import (
 // Register will be called in main and register all validation handlers
 func Register(mgr manager.Manager, args controllerCommon.Args) {
 	server := mgr.GetWebhookServer()
-	server.Register("/validating-v1alpha1-clusterresource", &webhook.Admission{Handler: &cluster_resource.ValidatingAdmission{}})
-	server.Register("/validating-v1alpha1-multiclusterresourceaggregaterules", &webhook.Admission{Handler: &cluster_resource_aggregate_rule.ValidatingAdmission{}})
+	server.Register("/validating-v1alpha1-clusterresource",
+		&webhook.Admission{Handler: &cluster_resource.ValidatingAdmission{}})
+	server.Register("/validating-v1alpha1-multiclusterresourceaggregaterules",
+		&webhook.Admission{Handler: &cluster_resource_aggregate_rule.ValidatingAdmission{}})
+	server.Register("/validating-v1alpha1-multiclusterresource",
+		&webhook.Admission{Handler: &multi_cluster_resource.ValidatingAdmission{}})
 
 }
